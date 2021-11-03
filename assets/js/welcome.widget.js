6c59b85808a5372d6dc0f3bcd62ea754f4bb6292
Aceius E <io@acei.us>
2021-11-03 20:37:28 +0000

    Select random translation from array

--- assets/js/welcome.widget.js
+++ assets/js/welcome.widget.js
@@ -1,5 +1,7 @@
 const greetings = ['Welcome to acei.us', 'Bienvenido a acei.us']
 
 const welcome_loop = window.setInterval(function(){
-    document.getElementById('greetings').innerHTML = Math.floor((Math.random() * greetings.length) + 1)
-}, 5000);
+    let selected_greeting = greetings[Math.floor(Math.random() * greetings.length)]
+    document.getElementById('greetings').innerHTML = selected_greeting
+    console.log('Selected greeting ' + selected_greeting)
+}, 3000);
