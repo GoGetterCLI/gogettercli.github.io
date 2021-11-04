9d2bb33ab78d130e2a0d6fc09875ab8411c4063d
Aceius E <io@acei.us>
2021-11-04 00:51:58 +0000

    Sequenced greeting order

--- assets/js/welcome.widget.js
+++ assets/js/welcome.widget.js
@@ -1,7 +1,12 @@
 const greetings = ['Welcome to acei.us', 'Bienvenido a acei.us']
+let selected_greeting = 0;
 
 const welcome_loop = window.setInterval(function(){
-    let selected_greeting = greetings[Math.floor(Math.random() * greetings.length)]
-    document.getElementById('greetings').innerHTML = selected_greeting
-    console.log('Selected greeting ' + selected_greeting)
+    document.getElementById('greetings').innerHTML = greetings[selected_greeting]
+    selected_greeting += 1;
+    if (selected_greeting > greetings.length) {
+        selected_greeting = 0;
+        console.log('Restarted Greeting Loop')
+    }
+    console.log('Selected greeting ' + (selected_greeting + 1))
 }, 3000);
