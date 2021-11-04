f661817248b8b0e807b5f31dd825e88471a2003e
Aceius E <io@acei.us>
2021-11-04 16:10:04 +0000

    Added Mandarin to translations on homepage - 歡迎來到acei.us

--- assets/js/welcome.widget.js
+++ assets/js/welcome.widget.js
@@ -1,13 +1,15 @@
-const greetings = ['Welcome to acei.us', 'Bienvenido a acei.us']
+const greetings = ['Welcome to acei.us', 'Bienvenido a acei.us', '歡迎來到acei.us']
 let selected_greeting = 0;
 
 const welcome_loop = window.setInterval(function(){
     document.getElementById('greetings').innerHTML = greetings[selected_greeting]
-    if (selected_greeting = greetings.length) {
+    
+    if (greetings.length == (selected_greeting + 1)) {
         selected_greeting = 0;
-        console.log('Restarted Greeting Loop');
+        console.log("[Debug] selected_greeting = " + selected_greeting);
     } else {
         selected_greeting += 1;
-        console.log('Selected greeting ' + (selected_greeting));
+        console.log("[Debug] selected_greeting = " + selected_greeting);
     }
+    
 }, 3000);
