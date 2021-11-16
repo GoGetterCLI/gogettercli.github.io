5d8d68192717d48a92d29459c790ba5db8d51fb2
Aceius E <io@acei.us>
2021-11-16 18:10:35 +0000

    Add 🐢tortle playground

---
+++ assets/js/sketch-playground.widget.js
@@ -0,0 +1,4 @@
+const sketch_playground = doucment.getElementById('sketch_playground');
+const turtle = sketch_playground.getContext('2d')
+
+document.getElementById("sketch_container").innerHTML = '<canvas id="sketch_playground"></canvas>'
