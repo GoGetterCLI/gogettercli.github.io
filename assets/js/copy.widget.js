8138b2da83e2c7fb9590011f6bdbe316621629ff
Aceius E <io@acei.us>
2021-11-17 19:06:05 +0000

    Change API Call here

--- assets/js/copy.widget.js
+++ assets/js/copy.widget.js
@@ -1,5 +1,6 @@
 function copy() {
     //const copyText = "git@staging.acei.us" 
     let copyText = document.getElementById('email').innerHTML;
-    navigator.clipboard.writeText(copyText);
+    //navigator.clipboard.writeText(copyText);
+    navigator.clipboard.copy(copyText);
   }
