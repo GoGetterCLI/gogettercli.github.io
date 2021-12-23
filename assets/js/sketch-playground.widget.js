6ddadb9d51b7aeba0671cbb4ca346d5f23201d59
Aceius E <io@acei.us>
2021-12-23 11:27:58 -0800

    cleaning up some stuffs

--- assets/js/sketch-playground.widget.js
+++
@@ -1,4 +0,0 @@
-const sketch_playground = doucment.getElementById('sketch_playground');
-const turtle = sketch_playground.getContext('2d')
-
-document.getElementById("sketch_container").innerHTML = '<canvas id="sketch_playground"></canvas>'
